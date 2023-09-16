c11dcbd1e1b8cdb3d22c8e4bba19934fb86c0a48
duoduo6666 <2326064185@qq.com>
2023-09-16 15:24:10 +0000

    2023-09-16 23:24:10

--- krpc1.py
+++ krpc1.py
@@ -65,14 +65,50 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     call1.service = "SpaceCenter"
     call1.procedure = "get_UT"
     request.calls.append(call1)
-    
+
     send_msg(s, request)
     r = recv_msg(s, krpc_pb2.Response)
     status = krpc_pb2.Status()
     status.ParseFromString(r.results[0].value)
-    Response = decode_double(r.results[1].value)
+    ut = decode_double(r.results[1].value)
+
+    request = krpc_pb2.Request()
+    call2 = krpc_pb2.ProcedureCall()
+    call2.service = "SpaceCenter"
+    call2.procedure = "get_ActiveVessel"
+    request.calls.append(call2)
+    send_msg(s, request)
+    r = recv_msg(s, krpc_pb2.Response)
+    Vessel = r.results[0].value
+
+    request = krpc_pb2.Request()
+    call3 = krpc_pb2.ProcedureCall()
+    call3.service = "SpaceCenter"
+    call3.procedure = "Vessel_Flight"
+    argv1 = krpc_pb2.Argument()
+    argv1.position = 0
+    argv1.value = Vessel
+    call3.arguments.append(argv1)
+    request.calls.append(call3)
+    send_msg(s, request)
+    r = recv_msg(s, krpc_pb2.Response)
+    Flight = r.results[0].value
+
+    request = krpc_pb2.Request()
+    call4 = krpc_pb2.ProcedureCall()
+    call4.service = "SpaceCenter"
+    call4.procedure = "Flight_get_SurfaceAltitude"
+    argv1 = krpc_pb2.Argument()
+    argv1.position = 0
+    argv1.value = Flight
+    call4.arguments.append(argv1)
+    request.calls.append(call4)
+    send_msg(s, request)
+    r = recv_msg(s, krpc_pb2.Response)
+    h = decode_double(r.results[0].value)
 
     print(status.version)
-    print(Response)
-
-    
+    print(ut)
+    print(Vessel)
+    print(Flight)
+    print(h)
